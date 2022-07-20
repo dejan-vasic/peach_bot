a8824cb3a1d0ccccc0372672b39979e0ddcd4502
dejan-vasic <dejan.vasic2205@gmail.com>
2022-07-20 09:36:18 +0200

    code in line

--- peach_bot.py
+++ peach_bot.py
@@ -1,5 +1,6 @@
 import os
 import sys
+from textwrap import indent
 import discord
 
 from dotenv import load_dotenv
@@ -11,47 +12,56 @@ from contextlib import redirect_stderr, redirect_stdout
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-bot = commands.Bot(command_prefix='*')
-cli = discord.Client()
-
-@bot.event
-async def on_ready():
-    print(f'{bot.user.name} has connected to Discord!')
+client = discord.Client()
 
 lines = ''
-last_msg = None
+indent_mode = False
 
-def read_from(path):
-    f = open('to_do.txt', 'r')
-    return f.read()
+@client.event
+async def on_ready():
+    print(f'{client.user.name} has connected to Discord!')
 
-def write_to(path, content):
-    f = open('to_do.txt', 'a')
-    f.write(content)
+@client.event
+async def on_message(message):
+    global lines
+    global indent_mode
+    if message.author == client.user:
+        return
 
-@bot.command(name='py')
-async def execute(ctx, *args):
-    global last_msg
-    async for message in ctx.history(limit = 1000):
-        if '```py\n# ' + args[0] in message.content:
-            codeString = message.content.strip('```')
-            codeString = codeString.strip('py')
-            codeObject = compile(codeString, 'sumstring', 'exec')
-            redirected_output = sys.stdout = StringIO()
-            exec(codeObject)
-            if redirected_output.getvalue() and args[1:]:
-                await clear(ctx, '*')
-                if last_msg:
-                    await last_msg.delete()
-                last_msg = await ctx.send(f'> **__todo__**'.upper() + f':\n```{redirected_output.getvalue()}```')
-            elif redirected_output.getvalue():
-                await ctx.send(f'> **__output__**'.upper() + f':\n```{redirected_output.getvalue()}```')
+    if 'q' in message.content:
+        await message.delete()
+        redirected_output = sys.stdout = StringIO()
+        try:
+            exec(lines, globals())
+        except Exception as err:
+            await message.channel.send(f'`{err}`')
+        if redirected_output.getvalue():
+            await message.channel.send(f'`{redirected_output.getvalue()}`')
+        
+        indent_mode = False
 
-@bot.command(name='cls')
-async def clear(ctx, *args):
-    async for message in ctx.history(limit = 1000):
-        if not args and not message.content.startswith('```py'):
-            await message.delete()
-        if args and args[0] == '*' and message.content.startswith('*'):
-            await message.delete()
-bot.run(TOKEN)
+    message_start = message.content.startswith('`')
+    message_end = message.content.endswith('`') 
+
+    if message_start and message_end:
+        line = message.content.strip('`')
+        if not indent_mode:
+            lines = line + '\n'
+        redirected_output = sys.stdout = StringIO()
+
+        if line.endswith(':'):
+            indent_mode = True
+            return
+
+        if indent_mode:
+            lines += line + '\n'
+            return
+
+        try:
+            exec(lines, globals())
+        except Exception as err:
+            await message.channel.send(f'`{err}`')
+        if redirected_output.getvalue():
+            await message.channel.send(f'`{redirected_output.getvalue()}`')
+
+client.run(TOKEN)
