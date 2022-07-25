b876f89d09b9af07cd4e6bf52a36598b4ea57293
dejan-vasic <dejan.vasic2205@gmail.com>
2022-07-25 17:40:17 +0200

    'cleaner'

--- peach_bot.py
+++ peach_bot.py
@@ -1,67 +1,87 @@
+import re
 import os
 import sys
-from textwrap import indent
+from io import StringIO
 import discord
 
 from dotenv import load_dotenv
-from discord.ext import commands
-
-from io import StringIO
-from contextlib import redirect_stderr, redirect_stdout
+#from discord.ui import Button, View
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-client = discord.Client()
+intents = discord.Intents.all()
+client = discord.Client(intents=intents)
 
-lines = ''
-indent_mode = False
+lines = []
+loops = ('for', 'while')
 
 @client.event
 async def on_ready():
     print(f'{client.user.name} has connected to Discord!')
 
+#region Discord
+@client.event
+async def on_message_edit(before, after):
+
+    msg = after.content
+    if msg.startswith('`') and msg.endswith('`'):
+        line = msg.strip('`')
+        if line.startswith('py'):
+            line = line.strip('py')
+        if line.startswith(loops):
+            pass
+        lines.append(line)
+        out = sys.stdout = StringIO()
+        try:
+            exec(line, globals())
+        except Exception as err:
+            await after.channel.send(f'`{err}`')
+        if out.getvalue():
+            await after.channel.send(f'`{out.getvalue()}`')
+
+
 @client.event
 async def on_message(message):
-    global lines
-    global indent_mode
     if message.author == client.user:
         return
 
-    if 'q' in message.content:
-        await message.delete()
-        redirected_output = sys.stdout = StringIO()
+    msg = message.content
+    if msg.startswith('`') and msg.endswith('`'):
+        line = msg.strip('`')
+        if line.startswith('py'):
+            line = line.strip('py')
+        if line.startswith(loops):
+            pass
+        lines.append(line)
+        out = sys.stdout = StringIO()
         try:
-            exec(lines, globals())
+            exec(line, globals())
         except Exception as err:
             await message.channel.send(f'`{err}`')
-        if redirected_output.getvalue():
-            await message.channel.send(f'`{redirected_output.getvalue()}`')
-        
-        indent_mode = False
+        if out.getvalue():
+            await message.channel.send(f'`{out.getvalue()}`')
+#endregion
 
-    message_start = message.content.startswith('`')
-    message_end = message.content.endswith('`') 
 
-    if message_start and message_end:
-        line = message.content.strip('`')
-        if not indent_mode:
-            lines = line + '\n'
-        redirected_output = sys.stdout = StringIO()
+#region Tests
+# @client.event
+# async def on_message(message):
+#     if message.author == client.user:
+#         return
+    
+#     button = Button(
+#         label='hello',
+#         style=discord.ButtonStyle.blurple
+#     )
 
-        if line.endswith(':'):
-            indent_mode = True
-            return
+#     async def button_callback(ineraction):
+#         await ineraction.response.send_message(button.label)
 
-        if indent_mode:
-            lines += line + '\n'
-            return
-
-        try:
-            exec(lines, globals())
-        except Exception as err:
-            await message.channel.send(f'`{err}`')
-        if redirected_output.getvalue():
-            await message.channel.send(f'`{redirected_output.getvalue()}`')
+#     button.callback = button_callback
+#     view = View()
+#     view.add_item(button)
+#     await message.channel.send(view=view)
+#endregion
 
 client.run(TOKEN)
